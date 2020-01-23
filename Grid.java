c1cd4887a5278e3eedee771e6a1ae2a604408950
dperfetti21 <dperfetti21@scarsdaleschools.org>
2020-01-23 09:45:25 -0500

    grid

--- Grid.java
+++ Grid.java
@@ -1,4 +1,3 @@
-
 /**
  * 
  * 
@@ -11,21 +10,37 @@ public class Grid
   
     public void gridOne ()
     {
-        grid = new char [4][4];
-        for(int r =0; r<grid.length; r++)
+        grid = new char [10][25];
+        int r = 0;
+        //set each box to a space
+        for(int x =0; x<grid.length; x++)
         {
-            for(int c =0; c < grid[r].length; c++)
+            for(int c =0; c< grid[x].length; c++)
             {
-                grid[r][c] = '*';
-                grid[0][0] = 'o';
-                grid[0][3] = 'o';
-                grid[3][0] = 'o';
-                grid[3][3] = 'o';
-                System.out.print(grid[r][c] + " ");
+                grid[r][c] = 'X';
+            }
+        }
+        //fill top and bottom 
+        for(int c=0; c< grid[r].length; c++)
+        {
+            grid[r][c] = '*';
+            grid[9][c] = '*';
+        }
+        //fill sides 
+        for(int row = 0; row < grid.length; row++)
+        {
+            grid[row][0] = '*';
+            grid[row][24] = '*';
+        }
+        grid[0][0] = 'o';
+        
+        for(int x =0; x < grid.length; x++)
+        {
+            for(int c =0; c < grid[x].length; c++)
+            {
+                System.out.print(grid[x][c] + " ");
             }
             System.out.println("");
         }
     }
-
-   
 }
